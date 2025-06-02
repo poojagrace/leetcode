b5e52242d80435a8b37e7ec6155bdbcfa8a487ac
PoojaGrace <poojacholla75@gmail.com>
2025-06-02 08:05:55 +0530

    Time: 0 ms (100%), Space: 42.5 MB (52.58%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,19 @@
+
+ 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode node = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;        
+    }
+}
+        
+    
